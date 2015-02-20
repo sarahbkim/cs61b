90fb56651728f6eb1afdd4373d477e2eec8dd60d
sarahbkim <pnkdaegee@gmail.com>
2015-02-19 19:48:12 -0800

    smoosh method added

--- src/main/java/Hw_3/SListNode.java
+++ src/main/java/Hw_3/SListNode.java
@@ -1,6 +1,5 @@
 package Hw_3;
 /* SListNode.java */
-
 /**
  *  SListNode is a class used internally by the SList class.  An SList object
  *  is a singly-linked list, and an SListNode is a node of a singly-linked
@@ -14,6 +13,15 @@ class SListNode {
     Object item;
     SListNode next;
 
+    /**
+     *  SListNode() (with one parameter) constructs a list node referencing the
+     *  item "obj".
+     */
+
+    SListNode(Object obj) {
+        item = obj;
+        next = null;
+    }
 
     /**
      *  SListNode() (with two parameters) constructs a list node referencing the
@@ -25,12 +33,4 @@ class SListNode {
         this.next = next;
     }
 
-    /**
-     *  SListNode() (with one parameter) constructs a list node referencing the
-     *  item "obj".
-     */
-
-    SListNode(Object obj) {
-        this(obj, null);
-    }
 }
