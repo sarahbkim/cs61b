abc867de428a8933f2362707706c27a1c0767eb5
sarahbkim <pnkdaegee@gmail.com>
2015-02-24 23:00:26 -0800

    start PixImage

--- src/main/java/Lab_5/Animal.java
+++ src/main/java/Lab_5/Animal.java
@@ -4,4 +4,54 @@ package Lab_5;
  * Created by sarahbkim on 2/22/15.
  */
 public class Animal {
+    protected int legs;
+    protected String name;
+    protected int age;
+    public static final int ERROR_CODE = 123;
+
+    /* constructors */
+    public Animal(String name, int legs, int age) {
+        this.name = name;
+        this.legs = legs;
+        this.age = age;
+    }
+
+    public Animal(String name, int age) {
+        this(name, 4, age); // make legs default to 4
+    }
+
+    public Animal() {
+        this("None", 4, 0);
+    }
+
+    /* get name */
+    public String getName() {
+        return name;
+    }
+
+    /* get age */
+    public int getAge() {
+        return age;
+    }
+
+    /* get num of legs */
+    public int getNumberOfLegs() {
+        return legs;
+    }
+
+    /* get dog years */
+    public int ageInAnimalYears(String animalType) {
+        System.out.println("Original method!");
+        if(animalType.equals("Dog")){
+            return age * 7;
+        } else {
+            return age * 30;
+        }
+    }
+
+    public static void main(String[] args) {
+    System.out.println(ERROR_CODE);
+
+
+    }
 }
--- src/main/java/Lab_5/Dog.java
+++ src/main/java/Lab_5/Dog.java
@@ -3,5 +3,88 @@ package Lab_5;
 /**
  * Created by sarahbkim on 2/22/15.
  */
-public class Dog {
+public class Dog extends Animal{
+    protected String greeting;
+    public static final int ERROR_CODE = 123;
+
+    /* constructors */
+    public Dog(String name, int legs, int age, String greeting) {
+        super(name, legs, age); // this calls the Animal constructor with all these args
+        this.greeting = greeting;
+    }
+
+    public Dog() {
+    }
+
+    /* get age */
+    public int getAge() {
+        return age;
+    }
+
+    public int ageInAnimalYears(String animal) {
+        super.ageInAnimalYears(animal);
+        if(animal.equals("Dog")){
+            return age * 7;
+        } else {
+            return age * 30;
+        }
+    }
+
+    public static void main(String[] args) {
+        if(ERROR_CODE==123) {
+            System.out.println("error!");
+        } else {
+            System.out.println("no error!");
+        }
+
+        Dog fido = new Dog("Fido", 4, 1, "Yip");
+        System.out.println(fido.getName());
+
+
+        ((Animal) fido).ageInAnimalYears("Dog");
+        Animal original = new Dog();
+        ((Animal) original).ageInAnimalYears("Dog");
+
+        fido.ageInAnimalYears("Dog");
+
+        // learning about inheritance...
+        Animal greg = new Dog();
+        System.out.println(greg.getName());
+
+        // Downcasting...
+        Animal y = new Dog();
+        Dog test = (Dog)y;
+
+        Animal c = new Animal();
+
+        // an array of objects
+        Dog[] xa = new Dog[4];
+        for(int i=0;i<4;i++) {
+            xa[i] = new Dog();
+        }
+
+        Animal[] ya = new Animal[4];
+        for(int i=0;i<4;i++) {
+            ya[i] = new Animal();
+        }
+
+        Dog[] xb = new Dog[4];
+        for(int i=0;i<4;i++){
+            Animal z = new Dog();
+            xb[i] = (Dog) z;
+        }
+
+        ya = xa;
+        xa = (Dog[]) ya;
+
+        Animal s;
+        Dog t = new Dog();
+        s = t;
+        t = (Dog)s;
+
+        s = new Animal();
+        t = (Dog) s; // Run-time errord
+
+
+    }
 }
