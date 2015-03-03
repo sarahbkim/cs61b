c87571157a9364e0b66ec610dbb24a3e9d7a535d
sarahbkim <pnkdaegee@gmail.com>
2015-03-02 22:46:53 -0800

    hw 4 part 1

--- src/main/java/Hw_4/List/DList.java
+++ src/main/java/Hw_4/List/DList.java
@@ -51,6 +51,8 @@ public class DList {
      */
     public DList() {
         //  Your solution here.
+        this.head = new DListNode(null, head, head);
+        size = 0;
     }
 
     /**
@@ -77,7 +79,12 @@ public class DList {
      *  Performance:  runs in O(1) time.
      */
     public void insertFront(Object item) {
-        // Your solution here.
+        DListNode n = newNode(item, this.head, this.head.next);
+        if(size==0) {
+            this.head.prev = n;
+        }
+        this.head.next = n;
+        size++;
     }
 
     /**
@@ -86,7 +93,15 @@ public class DList {
      *  Performance:  runs in O(1) time.
      */
     public void insertBack(Object item) {
-        // Your solution here.
+        DListNode n = newNode(item, this.head.prev, this.head);
+        // original last item
+        if(size==0){
+            this.head.next = n;
+        } else {
+            this.head.prev.next = n;
+        }
+        this.head.prev = n;
+        size++;
     }
 
     /**
@@ -99,7 +114,10 @@ public class DList {
      *  Performance:  runs in O(1) time.
      */
     public DListNode front() {
-        // Your solution here.
+        if(size==0){
+            return null;
+        }
+        return head.next;
     }
 
     /**
@@ -112,7 +130,10 @@ public class DList {
      *  Performance:  runs in O(1) time.
      */
     public DListNode back() {
-        // Your solution here.
+        if(size==0){
+            return null;
+        }
+        return head.prev;
     }
 
     /**
@@ -126,7 +147,19 @@ public class DList {
      *  Performance:  runs in O(1) time.
      */
     public DListNode next(DListNode node) {
-        // Your solution here.
+        if(node==null | node.next == head | node.next == null) {
+            return null;
+        }
+        DListNode current = head;
+        DListNode next = null;
+        while(current.next!=head) {
+            if(node==current){
+                next = current.next;
+                break;
+            }
+            current = current.next;
+        }
+        return next;
     }
 
     /**
@@ -140,7 +173,20 @@ public class DList {
      *  Performance:  runs in O(1) time.
      */
     public DListNode prev(DListNode node) {
-        // Your solution here.
+        if(node==null | node.prev == head) {
+            return null;
+        }
+        DListNode previous = null;
+        DListNode current = head;
+        while(current.prev != head){
+            if(node==current){
+                previous = current.prev;
+                break;
+            }
+            current = current.prev;
+        }
+
+        return previous;
     }
 
     /**
@@ -151,7 +197,10 @@ public class DList {
      *  Performance:  runs in O(1) time.
      */
     public void insertAfter(Object item, DListNode node) {
-        // Your solution here.
+        if(node==null){return;};
+        DListNode newNode = new DListNode(item, node, node.next);
+        node.next.prev = newNode;
+        node.next = newNode;
     }
 
     /**
@@ -163,6 +212,10 @@ public class DList {
      */
     public void insertBefore(Object item, DListNode node) {
         // Your solution here.
+        if(node==null){return;}
+        DListNode newNode = new DListNode(item, node.prev, node);
+        node.prev.next = newNode;
+        node.prev = newNode;
     }
 
     /**
@@ -170,7 +223,11 @@ public class DList {
      *  Performance:  runs in O(1) time.
      */
     public void remove(DListNode node) {
-        // Your solution here.
+        if(node==null){return;}
+        // update the previous node
+        node.prev.next = node.next;
+        // update the next node
+        node.next.prev = node.prev;
     }
 
     /**
@@ -189,5 +246,51 @@ public class DList {
             current = current.next;
         }
         return result + "]";
+    }
+
+    public static void main(String[] args){
+        DList d = new DList();
+        System.out.println("Testing insertBack method: ");
+        d.insertBack(1);
+        d.insertBack(2);
+        d.insertBack(3);
+        d.insertBack(4);
+
+        String s = d.toString();
+        System.out.println(s);
+
+        System.out.println("Testing insertFront method:");
+        d.insertFront(10);
+        d.insertFront(9);
+
+        String s2 = d.toString();
+        System.out.println(s2);
+
+        System.out.println("Testing back() method: ");
+        DListNode x = d.back();
+        System.out.println("x should equal 4: " + x.item);
+        DListNode f = d.front();
+        System.out.println("f should equal 9: " + f.item);
+
+        System.out.println("Testing prev method: ");
+        DListNode p = d.prev(x);
+        System.out.println("p should equal 3" + p.item);
+
+
+        System.out.println("Testing insertAfter method: ");
+        d.insertAfter(5, d.back());
+        String s3 = d.toString();
+        System.out.println(s3);
+        System.out.println(d.back().item);
+        System.out.println("Testing insertBefoe method: ");
+        d.insertBefore(11, d.back());
+        String s4 = d.toString();
+        System.out.println(s4);
+        System.out.println("Testing delete method: ");
+        d.remove(d.front());
+        d.remove(d.back());
+        String s5 = d.toString();
+        System.out.println(s5);
+
     }
 }
