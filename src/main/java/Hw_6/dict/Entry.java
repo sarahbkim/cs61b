bf9e8767c3d3d6f43dec05caaf05cc8f4d938316
sarahbkim <pnkdaegee@gmail.com>
2015-04-05 20:16:34 -0700

    add new starter files

---
+++ src/main/java/Hw_6/dict/Entry.java
@@ -0,0 +1,25 @@
+package main.java.Hw_6.dict;
+/* Entry.java */
+
+
+/**
+ *  A class for dictionary entries.
+ *
+ *  DO NOT CHANGE THIS FILE.  It is part of the interface of the
+ *  Dictionary ADT.
+ **/
+
+public class Entry {
+
+    protected Object key;
+    protected Object value;
+
+    public Object key() {
+        return key;
+    }
+
+    public Object value() {
+        return value;
+    }
+
+}
